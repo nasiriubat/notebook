57dcf5418c24a0dcbe11601721f90ea6b8d879cf
nasiriubat <shuvo131662@gmail.com>
2025-04-08 07:05:07 +0300

    shell file updated

--- update.sh
+++ update.sh
@@ -3,16 +3,21 @@ set -e
 echo "🔄 Updating application..."
 
 # Pull latest code
+git reset --hard HEAD
 git pull origin main
+
 
 # Backend update
 cd backend
 source venv/bin/activate
 pip install -r requirements.txt
+flask db upgrade
 pm2 restart flask-backend
 
 # Frontend update
 cd ../frontend
+rm -rf node_modules
+rm -rf package-lock.json
 npm install
 npm run build
 pm2 restart vite-frontend
